84e49af3a081ed76c5dcf65f67a24d56e30159f6
Otto Seiskari <otto.seiskari@gmail.com>
2021-10-29 21:23:34 +0300

    Add scripts for importing (Python docs)

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
+# Docs website
+
+A static website hosting the documentation of the Spectacular AI SDK
+see also https://github.com/SpectacularAI/sdk-examples.
+
+## Updating
+
+Run this script on a file that contains a new version of the Python plugin
+
+    ./import-python-docs.sh python-plugin.zip
+
+Then check
+
+    git status
+
+If there are changes, commit and push them.
+
 # Spectacular AI demo GIFs
+
+(also hosted on the same site)
 
 See https://github.com/SpectacularAI/sdk-examples for the example
 codes which were used to create these videos.
---
+++ import-python-docs.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -eux
+TARGET="$1"
+WORK=$(mktemp -d)
+CUR=`pwd`
+cp "$TARGET" "$WORK"
+cd "$WORK"
+unzip *.zip
+
+MINOR_VERSION=$(
+  find wheels/ |
+  grep spectacularAI- |
+  head -n 1 |
+  awk 'BEGIN { FS = "-" }; { print $2; }' |
+  awk 'BEGIN { FS = "." }; { print $1 "." $2; }')
+
+cat > most_recent_version.py << 'EOF'
+import sys
+def version_to_num(s):
+    weight = 1.0
+    num = 0.0
+    for v in s.strip().replace('v', '').split('.'):
+        num += int(v) * weight
+        weight /= 1000.0
+    return num
+
+versions = sorted(sys.stdin.readlines(), key=version_to_num)
+print(versions[-1].strip())
+EOF
+
+cd "$CUR"
+TARGET_DIR="sdk/python/v$MINOR_VERSION"
+rm -rf "$TARGET_DIR"
+cp -r "$WORK/docs" "$TARGET_DIR"
+
+MOST_RECENT=$(ls sdk/python | python "$WORK/most_recent_version.py")
+rm -rf "$WORK"
+rm -rf "sdk/python/latest"
+cp -r "sdk/python/$MOST_RECENT" "sdk/python/latest"
